ccb61663889a460cab1670261ed4f8da2d4042e8
Callum Terris <callum.terris@gmail.com>
2014-01-25 18:20:18 +0000

    Level Generation now generates the approprate obstacles based on the file read in.

--- GGJ14/Assets/Scripts/LevelGenerator.cs
+++ GGJ14/Assets/Scripts/LevelGenerator.cs
@@ -10,28 +10,28 @@ public class LevelGenerator : MonoBehaviour
     public int size = 40;
     public TextAsset level;
     List<FloorDrop> floor;
+    public GameObject plane;
+    int planeSize;
 
     void Awake ()
     {
+        //Debug.Log ("bit set {}".Format(BitIsSet(3, 1)));
         floor = new List<FloorDrop> ();
 
         if (level) {
             string[] levelSections = level.text.Split (',');
+            planeSize = levelSections.Length;
 
             bool first = true;
-            foreach (string s in levelSections)
-            {
+            foreach (string s in levelSections) {
                 if (!first)
-                    AddSection(floorPrefab);
+                    AddSection (floorPrefab, 0);
                 else
                     first = false;
-                if (s == "0")
-                {
-                    AddSection(floorPrefab);
-                }
-                else
-                {
-                    AddSection (wallPrefab);
+                if (s == "0") {
+                    AddSection (floorPrefab, 0);
+                } else {
+                    AddSection (wallPrefab, int.Parse (s));
                 }
             }
         } else {
@@ -42,13 +42,33 @@ public class LevelGenerator : MonoBehaviour
             }
         }
         StartCoroutine (StartDropDelayed ());
+        GameObject obj = GameObject.Instantiate (plane, new Vector3 ((3 * planeSize) / 2, -10, 0), Quaternion.identity) as GameObject;
+        obj.transform.localScale.Set (3 * planeSize, 1, 1);
     }
 
-    void AddSection (GameObject prefab)
+    void AddSection (GameObject prefab, int value)
     {
         GameObject go = GameObject.Instantiate (prefab, new Vector3 (floor.Count * 3, 0, 0), Quaternion.identity) as GameObject;
         FloorDrop section = go.GetComponent<FloorDrop> ();
         AddSection (section);
+
+        if (value > 0) {
+
+            int jumpHeight = 0;
+
+
+            ObsticleTriggerScript obsticleScript = go.GetComponentInChildren<ObsticleTriggerScript> ();
+            obsticleScript.Setup (BitIsSet (value, 1), BitIsSet (value, 2), BitIsSet (value, 3), BitIsSet (value, 0), 0);
+
+
+
+        }
+
+    }
+
+    bool BitIsSet (int val, int bit)
+    {
+        return (1 & (val >> bit)) == 1;
     }
 
     void AddSection (FloorDrop section)
