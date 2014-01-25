e5a2085e519354c7d73c7ca284d7476a9195fc52
dazedrobot <dazedrobot@gmail.com>
2014-01-25 03:14:05 +0000

    Added basic level Generator and dropping floors

---
+++ GGJ14/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public GameObject floorPrefab;
+    public GameObject wallPrefab;
+
+    public int size = 40;
+
+    List<FloorDrop> floor;
+
+	void Awake () {
+        floor = new List<FloorDrop> ();
+
+        FloorDrop previous = null;
+        for (int i =0; i < size; i++)
+        {
+            GameObject go = GameObject.Instantiate(floorPrefab, new Vector3(i*3,0,0), Quaternion.identity) as GameObject;
+            FloorDrop current = go.GetComponent<FloorDrop>();
+            if (previous)
+                previous.next = current;
+            previous = current;
+            floor.Add(current);
+        }
+        StartCoroutine (StartDropDelayed ());
+	}
+	
+	void Update () {
+	
+	}
+
+    IEnumerator StartDropDelayed()
+    {
+        yield return new WaitForSeconds (4);
+        floor [0].TriggerDrop ();
+    }
+}
