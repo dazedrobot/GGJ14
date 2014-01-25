f312fcb6a6da8975f7c1db70c9da71285c04dfa3
Emily Thomforde <emily@th.ingsmadeoutofotherthin.gs>
2014-01-25 19:54:51 +0000

    Colour and background and levels and things.

---
+++ GGJ14/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 position = transform.localPosition;
+		transform.localPosition = new Vector3 (target.transform.localPosition.x, position.y, position.z); 
+	}
+}
