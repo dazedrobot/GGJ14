279b7d83ab15d6f3106884ed60878995f9c9cc32
dazedrobot <dazedrobot@gmail.com>
2014-01-25 03:14:05 +0000

    Added basic level Generator and dropping floors

---
+++ GGJ14/Assets/Scripts/FloorDrop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorDrop : MonoBehaviour {
+
+    public FloorDrop next;
+
+    public void TriggerDrop()
+    {
+        StartCoroutine (Drop ());
+    }
+
+    IEnumerator Drop()
+    {
+        yield return new WaitForSeconds (1);
+        rigidbody2D.isKinematic = false;
+        rigidbody2D.fixedAngle = false;
+
+        if (next)
+            next.TriggerDrop();
+    }
+}
